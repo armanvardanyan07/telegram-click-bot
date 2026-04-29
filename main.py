953d190b838bbd1e8dcf0e4ec0cad36eb94ec5d8
armanvardanyan07 <armanvard123321@gmail.com>
2026-04-29 20:24:08 +0400

    1.0.0

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import telebot
+from threading import Thread
+app = Flask(__name__)
+bot = telebot.TeleBot("8713167991:AAEGSRr8VfpsAJTxeqJpLZzV7nHHvmLoUL8")
+users = set()
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    users.add(message.chat.id)
+
+@app.route("/click")
+def click():
+    for user_id in users:
+        bot.send_message(user_id, "Button was pressed")
+    return "ok"
+if __name__ == "__main__":
+    Thread(target=bot.infinity_polling, daemon=True).start()
+    app.run()
